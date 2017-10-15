c4960b53070336e4c6f90ef1eea35ee67cb577f6
Junior Dallagnolo <dall0078@algonquinlive.com>
2017-10-15 00:04:45 -0400

    Initialize Basic Javascript file

---
+++ namespace.js
@@ -0,0 +1,19 @@
+var DALL0078 = {
+  init: function () {
+    let divBox = document.createElement("div");
+    divBox.classList.add("box");
+    divBox.textContent = "DALL0078";
+    document.getElementById("boxes").appendChild(divBox);
+
+    divbox.addEventListener("mouseover", mousFunc);
+    divbox.addEventListener("mouseout", mousFunc);
+    divbox.addEventListener("click", function (ev) {
+      ev.currentTarget.style.backgroundColor = "#000";
+      ev.currentTarget.style.color = "#FFF";
+      ev.currentTarget.style.border = "thick groove #666";
+    });
+  },
+  mousFunc: function (ev) {
+    ev.currentTarget.classList.toggle("highlight");
+  }
+}
